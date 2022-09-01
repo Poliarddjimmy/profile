657be6c9bf7e24749d154eea1eaf0a71bb49da93
Poliard Djimmy <poliarddjimmy@gmail.com>
2022-09-01 11:16:10 -0400

    hotfix

--- components/seo.tsx
+++ components/seo.tsx
@@ -13,11 +13,11 @@ export default function SEO({
     <meta name="title" property="og:title" content={`Djimmy Poliard | ${title}`} key="title" />
     <meta property="og:site_name" content="Djimmy Poliard" key="site_name" />
     <meta property="og:url" content={'djimmypoliard.dev' + router.pathname} key="url" />
-    <meta property="og:description" content={description ? description : `Full-stack Engineer with ${(new Date()).getFullYear() - 2015}+ years of experience building scalable web applications and internal tools that drastically improve efficiency. Throughout my career I've built applications like fintech platform, communication, e-commerce, Point Of Sale(POS), HR management, School management, Social network and so on using PHP, laravel, Ruby on Rails, Sinatra, Bootstrap, Javascript, React JS, React Native, Next.Js, TypeScript GraphQL and other technologies`} key="description" />
+    <meta property="og:description" content={description ? description : `Full-stack Sotfware Engineer with ${(new Date()).getFullYear() - 2015}+ years of experience building scalable web applications and internal tools that drastically improve efficiency. Throughout my career I've built applications like fintech platform, communication, e-commerce, Point Of Sale(POS), HR management, School management, Social network and so on using PHP, laravel, Ruby on Rails, Sinatra, Bootstrap, Javascript, React JS, React Native, Next.Js, TypeScript GraphQL and other technologies`} key="description" />
     <meta property="og:type" content="website" key="type" />
     <meta name="keywords" content="curri culum, poliard, djimmy, poliard djimmy, djimmy poliard, jimmy, cv, portfolio, web, web dev, full, full stack, software" key="keywords" />
 
-    <meta name="image" property="og:image" content="/img/jey.jpg" key="image" />
+    <meta name="image" property="og:image" content="/images/mee.png" key="image" />
     <meta property="og:image:width" content="300" key="og:image:width" />
     <meta property="og:image:height" content="300" key="og:image:height" />
 
@@ -28,7 +28,7 @@ export default function SEO({
     <meta name="twitter:site" content="@poliard88" />
     <meta name="twitter:title" content={`Djimmy Poliard | ${title}`} />
     <meta name="twitter:description" content={description ? description : `Full-stack Engineer with ${(new Date()).getFullYear() - 2015}+ years of experience building scalable web applications and internal tools that drastically improve efficiency. Throughout my career I've built applications like fintech platform, communication, e-commerce, Point Of Sale(POS), HR management, School management, Social network and so on using PHP, laravel, Ruby on Rails, Sinatra, Bootstrap, Javascript, React JS, React Native, Next.Js, TypeScript GraphQL and other technologies`} />
-    <meta name="twitter:image" content="/img/me.png" />
+    <meta name="twitter:image" content="/images/mee.png" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
