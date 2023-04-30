0d9ba0ae3820c5ad6715ec5d4f385377ea894928
Djimmy Poliard <djimmypoliard@gmail.com>
2023-04-30 01:10:48 -0400

    remove stuffs

--- utils/graphqlClient.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from "graphql-request"
-
-import { GRAPHQL_API_URL } from "./constants"
-
-const graphqlClient = new GraphQLClient(GRAPHQL_API_URL, { headers: {hostname: typeof window !== 'undefined' ? window.location.hostname : '',} })
-
-export { graphqlClient }
