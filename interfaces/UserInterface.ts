3b66aed5d26908dc2e5b2d3ea0d95e702eea9ace
Djimmy Poliard <djimmypoliard@gmail.com>
2023-04-30 01:05:04 -0400

    remove stuffs

--- interfaces/UserInterface.ts
+++
@@ -1,8 +0,0 @@
-export default interface UserInterface {
-    id: number
-    uuid: string
-    fullName: string
-    email: string
-    firstName: string
-    lastName: string
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider } from "@apollo/client";
-import client from "../graphql/apollo-client";
 import { ToastProvider } from 'react-toast-notifications';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <ToastProvider placement='bottom-right'>
-        <Component {...pageProps} />
-      </ToastProvider>
-    </ApolloProvider>
+    <ToastProvider placement='bottom-right'>
+      <Component {...pageProps} />
+    </ToastProvider>
   )
 }
 
