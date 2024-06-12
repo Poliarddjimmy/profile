15cd274a47e5fca216b79e5d40d9ca2e51c9601b
Djimmy Poliard <djimmypoliard@gmail.com>
2024-06-11 20:08:30 -0400

    sd

--- pages/articles/[slug]/index.tsx
+++ pages/articles/[slug]/index.tsx
@@ -8,64 +8,68 @@ import { substr } from "../../../utils/substr"
 
 const Blog = () => {
     const router = useRouter();
-    const [post, setPost] = useState<BlogInterface>({title: '', content: '', image: ''} as BlogInterface)
+    const [post, setPost] = useState<BlogInterface>({ title: '', content: '', image: '' } as BlogInterface)
     const [isLoading, setLoading] = useState(true)
-    
-   
+
+
     useEffect(() => {
         let slug = router.query.slug
-    
-          fetch(`/api/post?slug=${slug}`)
+
+        fetch(`/api/post?slug=${slug}`)
             .then((res) => res.json())
             .then((data) => {
                 setPost(data.post)
-              setLoading(false)
+                setLoading(false)
             })
     }, [router.query.slug])
-    
+
     const recontent = (co: any) => {
         let re = co?.replace(/<p>/, '')
         return re?.substring(0, 100)
-    }    
-  
+    }
+
     return (
         <Layout title={post?.title} image={post?.image} description={recontent(post?.content)}>
-    
-          <div className="row content">
-            <LeftSide />
-            
-            <div className="col-lg-8 col-md-8 col-xl-8 right-side pad-5 text-justify">
-                {isLoading ? (
-                    <div className="spinner-border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </div>
-                ) : (
-                    <Fragment>
-                        { post?.image &&
-                            <div className="mb-4">
-                                <Image 
-                                    src={post?.image} 
-                                    alt={post?.title}
-                                    width={'100%'}
-                                    height={'100%'}
-                                    sizes="100vw"
-                                />
-                            </div>
-                        }
-                        
-                        <div className="border-top border-bottom text-center cursor-pointer mb-3" onClick={() => router.push('/articles')}>
-                            <i className="fa fa-arrow-left me-2"></i> See all articles
+
+            <div className="row content">
+                <LeftSide />
+
+                <div className="col-lg-8 col-md-8 col-xl-8 right-side pad-5 text-justify">
+                    {isLoading ? (
+                        <div className="spinner-border" role="status">
+                            <span className="sr-only">Loading...</span>
                         </div>
-                        
-                        <h2 className="text-left">{post?.title}</h2>
-                        
-                        <div className="mt-3" dangerouslySetInnerHTML={{ __html: post?.content}}></div>
-                    </Fragment>
-                
-                )}
+                    ) : (
+                        <Fragment>
+
+                            <div className="border-top border-bottom text-centers cursor-pointer mb-3" onClick={() => router.push('/articles')}>
+                                <i className="fa fa-arrow-left me-2"></i> See all articles
+                            </div>
+
+                            {post?.image &&
+                                <div className="mb-3">
+                                    <Image
+                                        src={post?.image}
+                                        alt={post?.title}
+                                        width={'100%'}
+                                        height={'100%'}
+                                        sizes="100vw"
+                                    />
+                                </div>
+                            }
+                            <div className="border-top border-bottom text-center cursor-pointer mb-3" onClick={() => router.push('/articles')} style={{ height: '20px' }}>
+                                {/* <i className="fa fa-arrow-left me-2"></i> See all articles */}
+                            </div>
+
+                            <h2 className="text-left">{post?.title}</h2>
+
+                            <div className="mt-3" dangerouslySetInnerHTML={{ __html: post?.content }}></div>
+                        </Fragment>
+
+                    )}
+                </div>
             </div>
-        </div>
-    </Layout>
+        </Layout>
     )
 }
 
